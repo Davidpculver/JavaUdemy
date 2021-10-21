66f133f1c8b56869b30776f77dca0eaeb2a5091f
David Culver <culver.david.p@gmail.com>
2021-10-20 21:52:18 -0500

    java udemy practice

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -1,8 +1,12 @@
 public class MegaBytesConverter {
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
+//1 mb = 1024 KB
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else {
 
-
+        }
     }
 
 }
