ebd9d0e3afaca856c7c50439e8d7c2db6a73d9cd
David Culver <culver.david.p@gmail.com>
2021-10-22 13:48:24 -0500

    java exercise

--- src/MegaBytesConverter.java
+++ src/MegaBytesConverter.java
@@ -2,11 +2,17 @@ public class MegaBytesConverter {
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
 //1 mb = 1024 KB
+        int mb;
         if (kiloBytes < 0){
             System.out.println("Invalid Value");
         } else {
-
+            mb = kiloBytes / 1024;
+            System.out.println(kiloBytes + " KB = " + mb + " MB and ");
         }
+    }
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(4000);
     }
 
 }
